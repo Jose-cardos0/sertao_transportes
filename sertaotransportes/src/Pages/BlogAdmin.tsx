064fad592e700a2c707a1f766a347ee1c88346a6
Jose Cardoso <josedeveloperJS@gmail.com>
2024-11-04 19:11:09 -0300

    correcao buttom sair

--- sertaotransportes/src/Pages/BlogAdmin.tsx
+++ sertaotransportes/src/Pages/BlogAdmin.tsx
@@ -21,22 +21,24 @@ export function BlogAdmin() {
   }
   return (
     <main
-      className="bg-custom-blue-dark h-screen
+      className="bg-custom-blue-dark min-h-screen
      flex items-center justify-center m-auto"
     >
       <section className="w-11/12">
-        <button
-          className="py-2 px-4 my-5 bg-custom-blue-dark 
+        <div className="w-full flex justify-end items-end ">
+          <button
+            className="py-2 px-4 my-5 bg-custom-blue-dark 
         rounded-lg font-bold text-white shadow-md
          shadow-black hover:bg-gray-100
           transition duration-300
            hover:text-custom-blue-dark"
-          onClick={signOutLogin}
-        >
-          Sair
-        </button>
+            onClick={signOutLogin}
+          >
+            Sair
+          </button>
+        </div>
 
-        <form className="w-full p-5 bg-white flex items-center justify-center shadow-md shadow-black">
+        <form className="w-full p-5 bg-white flex-col items-center justify-center shadow-md shadow-black">
           <div className="w-full flex-col ">
             <div>
               <h1
@@ -68,9 +70,22 @@ export function BlogAdmin() {
             </label>
             <label>
               <p className="font-righ text-custom-blue-dark">Titulo do post:</p>
-              <textarea name="" id=""></textarea>
+              <textarea
+                name=""
+                id=""
+                className="p-2 text-custom-blue-dark rounded-md outline-none bg-blue-100 w-full"
+              ></textarea>
             </label>
           </div>
+          <button
+            className="py-2 px-4 my-5 bg-custom-blue-dark 
+        rounded-lg font-bold text-white shadow-md
+         shadow-black hover:bg-gray-100
+          transition duration-300
+           hover:text-custom-blue-dark"
+          >
+            Enviar
+          </button>
         </form>
       </section>
     </main>
